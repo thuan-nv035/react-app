2ae1c72f85a473859b788cfdb6e71d5947cd47e8
thuan-nv035 <54436704+thuan-nv035@users.noreply.github.com>
2022-04-25 11:48:30 +0700

    delete real time

--- src/pages/todos/List.jsx
+++ src/pages/todos/List.jsx
@@ -1,32 +1,65 @@
-import React, {useEffect} from 'react';
-import {db} from "../../firebase";
-import {collection, getDocs} from 'firebase/firestore'
-import { Link } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { db } from "../../firebase";
+import {
+  collection,
+  getDocs,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  query,
+  limit,
+  orderBy,
+} from "firebase/firestore";
+import { Link } from "react-router-dom";
 
 const List = () => {
-    const [todos, setTodos] = React.useState([]);
-    useEffect(() => {
-        (async () => {
-            const collectionRef = collection(db, 'todos');
-            const collectionSnapShot = await getDocs(collectionRef);
-            const localTodos = [];
-            collectionSnapShot.forEach(doc => {
-                localTodos.push({id:doc.id,message:doc.data().message});
-            });
-            setTodos(localTodos);
-        })();
-    },[]);
-    return (
-        <div>
-            <ul>
-                {todos.map((todo,index)=>(
-                    <li key={index}>{todo.message}
-                    <Link to = {`/edit?id=${todo.id}`}>Edit</Link>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+  const [todos, setTodos] = React.useState([]);
+  let unsub = null;
+  useEffect(() => {
+    (async () => {
+      const collectionRef = collection(db, "todos");
+
+      // lay ve snapshot 1 lan
+      // const collectionSnapShot = await getDocs(collectionRef);
+
+      // subscribe vao observable cua database
+      // observable / pub-sub
+      const collectionQuery = query(collectionRef, limit(3), orderBy('message','desc'));
+      unsub = onSnapshot(collectionQuery, (snapshot) => {
+        console.log("co su thay doi du lieu");
+        const localTodos = [];
+        snapshot.forEach((doc) => {
+          localTodos.push({
+            id: doc.id,
+            message: doc.data().message,
+          });
+        });
+        setTodos(localTodos);
+      });
+      // const localTodos = [];
+      // collectionSnapShot.forEach(doc => {
+      //     localTodos.push({id: doc.id, message: doc.data().message});
+      // });
+    })();
+  }, []);
+
+  const deleteNote = async (id) => {
+    const docRef = doc(db, "todos", id);
+    await deleteDoc(docRef);
+  };
+  return (
+    <div>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo.message}
+            <Link to={`/edit?id=${todo.id}`}>Edit</Link>
+            <button onClick={() => deleteNote(todo.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default List;
